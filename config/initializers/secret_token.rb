dd7d963ad0a8ac486b3b5f4755b6cf65ee7b0321
Handle <handle@handle-OptiPlex-990.(none)>
2013-02-04 17:10:08 +0800

    Initial Project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsJenkinsAisa::Application.config.secret_token = 'af84436e3c2b8a8af99b06d1d023d2e6978a8a1313423fa2cb1d15cad9f1f7f44604cb86c02e119bbef90ea746c92b824fc605027228990b0adaefd91077e5b0'
