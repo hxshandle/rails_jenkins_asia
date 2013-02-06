4e9416e0d231dcaa95284404e003aa34a7ac3a74
Handle.huang <handle.huang@outlook.com>
2013-02-06 15:32:40 +0800

    Add new layout for devise controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  # Define different layout for devise
+  layout Proc.new { |controller| controller.devise_controller? ? 'devise' : 'application' }
+
 end
