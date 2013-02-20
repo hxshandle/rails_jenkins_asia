3ad878489b0efbe2126f1f58f592c06014575f35
Handle <Handle.huang0409@gamil.com>
2013-02-20 15:53:35 +0800

    add chosen select support

--- app/assets/javascripts/twitter-bootstrap.js
+++ app/assets/javascripts/twitter-bootstrap.js
@@ -1,15 +1,19 @@
 var Utils = {
   /**
-  * Initlizate
-  */
-  initlizate:function(root){
+   * Initlizate
+   */
+  initlizate: function (root) {
     root == root || document;
-    $("a[rel=popover]",root).popover();
-    $(".tooltip",root).tooltip();
-    $("a[rel=tooltip]",root).tooltip();
-    $('.chzn-select').chosen();
+    $("a[rel=popover]", root).popover();
+    $(".tooltip", root).tooltip();
+    $("a[rel=tooltip]", root).tooltip();
+    $('.chzn-select').each(function () {
+      $this = $(this);
+      var placeholderText = $this.attr('placeholder');
+      $this.chosen({placeholder_text: placeholderText});
+    });
     // For all datepicker field
-    $("input.datepicker",root).each(function(){
+    $("input.datepicker", root).each(function () {
       $(this).datepicker({
         altFormat: "yy-mm-dd",
         dateFormat: "yy-mm-dd",
@@ -18,44 +22,43 @@ var Utils = {
     });
   },
   /**
-  * Notify utils
-  */
-  pnotify:function(title,text,type){
+   * Notify utils
+   */
+  pnotify: function (title, text, type) {
     $.pnotify({
       title: title,
-      text:  text,
-      type:  type,
+      text: text,
+      type: type,
       sticker: false,
-      history:false,
-      animate_speed:'normal'
+      history: false,
+      animate_speed: 'normal'
     });
   }
 
 };
 
 
-
 // Provide HTML element replacement for data-remote ajax calls
-$(function() {
+$(function () {
   Utils.initlizate();
   // Reinitlizate for ajax HTML content
   $('[data-remote][data-replace]')
-    .data('type', 'html')
-    .on('ajax:success', function(event, data) {
-      var $this = $(this);
-      $($this.data('replace')).html(data);
-      $this.trigger('ajax:replaced');
-    })
-    .on('ajax:replaced',function(evt,data){
-      var $this = $(this);
-      Utils.initlizate($this.data('replace'));
-    });
+      .data('type', 'html')
+      .on('ajax:success', function (event, data) {
+        var $this = $(this);
+        $($this.data('replace')).html(data);
+        $this.trigger('ajax:replaced');
+      })
+      .on('ajax:replaced', function (evt, data) {
+        var $this = $(this);
+        Utils.initlizate($this.data('replace'));
+      });
 });
 
 // Flash message nofitication processor
-$(function(){
-  $('#flash-message p').each(function(){
+$(function () {
+  $('#flash-message p').each(function () {
     var $this = $(this);
-    Utils.pnotify($this.attr('class'),$this.text().replace(/\n/g,''),$this.attr('class'));
+    Utils.pnotify($this.attr('class'), $this.text().replace(/\n/g, ''), $this.attr('class'));
   });
-}); 
+});
--- app/controllers/admin/account_controller.rb
+++ app/controllers/admin/account_controller.rb
@@ -2,6 +2,7 @@ class Admin::AccountController < ApplicationController
   load_and_authorize_resource :user, :parent => false
   #devise authorition
   before_filter :authenticate_user!
+
   def index
     @users = User.all
     @user = User.new
